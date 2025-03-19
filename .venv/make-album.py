f39730cd7df8f1be959f30040a0f8a2091ab1c7a
Annie-anni <liud76942@gmail.com>
2025-03-19 14:17:43 +0900

    Initial Commit 2

--- .venv/make-album.py
+++ .venv/make-album.py
@@ -1,5 +1,7 @@
-def make_album(album_name,band_name):
-    print(f"The '{album_name}' album by {band_name} is very popular!")
+def make_album(album_name,band_name,quantity):
+    print(f"Wow the '{album_name.upper()}' album by {band_name.upper()} is very popular!")
+    print(f"There are has {quantity} songs,that's very nice!")
+
 while True:
     print(f"\nWhat is your favourite album?\n(enter 'q' anytime to quit)")
     album_name = input("Album name:")
@@ -8,4 +10,11 @@ while True:
     band_name = input("Band name:")
     if band_name == 'q':
         break
-    make_album(album_name,band_name)
+    quantity = int(input("How many songs are on this album?:"))
+    if quantity == 'q':
+        break
+
+    make_album(album_name,band_name,quantity)
+
+
+
