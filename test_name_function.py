8a1636f3a798e439c017a444fe02f230c3da8809
Annie-anni <liud76942@gmail.com>
2025-03-26 13:31:05 +0900

    Adding optional arguments

--- test_name_function.py
+++ test_name_function.py
@@ -7,5 +7,9 @@ class NamesTestCase(unittest.TestCase):
         formatted_name = get_formatted_name('liu','dan')
         self.assertEqual(formatted_name,'Liu Dan')
 
+    def test_first_last_middle_name(self):
+        formatted_name = get_formatted_name('wolfgang','mozart','amadeus')
+        self.assertEqual(formatted_name,'Wolfgang Amadeus Mozart')
+
 if __name__ == '__main__':
     unittest.main()
