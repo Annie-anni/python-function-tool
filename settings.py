c7c53300d5d9f02c49959d6518d2b69d5bd837e5
Annie-anni <liud76942@gmail.com>
2025-04-03 18:31:13 +0900

    Initial commit

---
+++ settings.py
@@ -0,0 +1,11 @@
+class Settings():
+    #A "class" that stores all settings
+    def __init__(self):
+        #Initialize game settings
+        #Screen settings
+        self.screen_width = 1000
+        self.screen_height = 600
+        self.bg_color = (230,230,230)
+
+        #ship settings
+        self.ship_speed_factor = 1.5
